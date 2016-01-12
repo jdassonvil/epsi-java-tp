2559c2ab93aa435683895660ff5d24735b9ad7d9
Jerome Dassonville <jerome.dassonville@worldline.com>
2016-01-12 14:56:59 +0100

    Add .project for easier Eclipse import
    Signed-off-by: Jerome Dassonville <jerome.dassonville@worldline.com>

--- src/myproject/service/AuthentificationService.java
+++ src/myproject/service/AuthentificationService.java
@@ -1,23 +1,24 @@
 package myproject.service;
 
-import myproject.data.DonneesUtilisateur;
+import myproject.data.DonnéesUtilisateur;
 import myproject.exception.UtilisateurInconnuException;
 import myproject.model.Utilisateur;
 
 public class AuthentificationService {
-
-	private DonneesUtilisateur donnéesUtilisateur = new DonneesUtilisateur();
-
-	public boolean login(String username, String password) {
-
+	
+	private DonnéesUtilisateur donnéesUtilisateur = new DonnéesUtilisateur();
+	
+	public boolean login(String username, String password){
+		
+		Utilisateur utilisateur;
 		try {
-			Utilisateur utilisateur = donnéesUtilisateur.trouverUtilisateur(username);
+			utilisateur = donnéesUtilisateur.trouverUtilisateur(username);
 			return utilisateur.getPassword().equals(password);
 
 		} catch (UtilisateurInconnuException e) {
 			return false;
 		}
-
+		
 	}
 
 }
